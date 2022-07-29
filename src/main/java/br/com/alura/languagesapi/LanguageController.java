ed6ba62e0be4dee05f23dd4e32d5619723835223
IsaPhipho <isaphiladelpho@gmail.com>
2022-07-29 18:48:57 +0100

    Sending data from Postman to MongoDB

--- src/main/java/br/com/alura/languagesapi/LanguageController.java
+++ src/main/java/br/com/alura/languagesapi/LanguageController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -16,6 +18,12 @@ public class LanguageController {
   public List<Language> getLanguages() {
     List<Language> languages = repository.findAll();
     return languages;
+  }
+
+  @PostMapping("/languages")
+  public Language postLanguage(@RequestBody Language language) {
+    Language saveLanguage = repository.save(language);
+    return saveLanguage;
   }
 }
 
